a807650aacb68682d3ef8f30574f95957d97b9ac
jounumar <44790212+jounumar@users.noreply.github.com>
2018-11-25 02:56:38 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** Arab Dark - مدة الرابط : يوم | عدد استخدامات الرابط : 100 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
+
+
+client.login("NTE2MDMxMzAzMjQ2ODA3MDQ2.DttzNQ.yqJ7J0LJmWFjJrKl8HB7sRhR290")
